64ac3caa8cdaa68f4964878f4b931e2e9b2cb49d
ishanvepa <ishanvepa171@gmail.com>
2025-06-14 19:13:20 -0400

    flask boilerplate and kmeans note_clustering configured

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,25 @@
 #intialize flask app and rest endpoints
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, origins=["http://localhost:8081"])
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Hello, world!"})
+
+@app.route('/new-note', methods=['POST'])
+def r_new_note():    
+    return jsonify({"message": "New note created"})
+
+@app.route('/delete-note', methods=['GET'])
+def r_delete_note():
+    return jsonify({"message": "Note deleted"})
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
